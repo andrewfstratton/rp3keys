a0eb7e639e48af6b519102fc2084899c330a03f4
andrewfstratton <andrewfstratton@gmail.com>
2024-07-30 14:50:45 +0100

    Tidy led constant

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module rp3keys
 
 go 1.22
 
-require (
-	tinygo.org/x/drivers v0.27.0
-)
+require tinygo.org/x/drivers v0.27.0
--- leds/leds.go
+++ leds/leds.go
@@ -8,7 +8,7 @@ import (
 	"tinygo.org/x/drivers/ws2812"
 )
 
-const GPIO_PIN = machine.GPIO18
+const ws2812Pin = machine.GPIO18
 
 var led ws2812.Device
 
@@ -16,7 +16,7 @@ var colours [3]color.RGBA
 var Off = color.RGBA{R: 0x00, G: 0x00, B: 0x00}
 
 func init() {
-	led = ws2812.NewWS2812(GPIO_PIN)
+	led = ws2812.NewWS2812(ws2812Pin)
 	led.Pin.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	go func() {
 		time.Sleep(time.Millisecond * 10)
